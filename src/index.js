48a2b2b0e8ce82cc88fa1f70e657de204f732cf3
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-16 16:18:13 +0300

    Rewrite code with linter 	rules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    const name = readlineSync.question('Tell me your name, please: ');
-    console.log(`Hello, ${name}!`);
-}
+const askName = () => {
+  const name = readlineSync.question('Tell me your name, please: ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default askName;
