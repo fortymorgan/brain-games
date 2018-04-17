d78473cbbc74dbbc3bd337968da70b67d1a20856
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-17 11:02:14 +0300

    Introduce oddOrEven brain-game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,24 @@
+import askName from '..';
+import { generateNumbers, question } from '../oddOrEven';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+
+const playerName = askName();
+
+console.log(`Let's play a game, ${playerName}!`);
+
+const numbers = generateNumbers();
+let totalCount = 0;
+
+for (let i = 1; i <= 3; i += 1) {
+  totalCount += question(numbers[i - 1]);
+  if (totalCount !== i) {
+    console.log(`Let's try again, ${playerName}!`);
+    break;
+  }
+}
+
+if (totalCount === 3) {
+  console.log(`Congratulations, ${playerName}!`);
+}
