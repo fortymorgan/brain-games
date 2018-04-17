c9bb9a2ec0e93d32f93a680725dbcfddfc69b5a0
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-17 13:17:47 +0300

    Move away all game logic from binary

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,25 +1,8 @@
 #!/usr/bin/env node
-import askName from '..';
-import { generateNumbers, question } from '../oddOrEven';
+import game from '..';
+import * as oddOrEven from '../oddOrEven';
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no".');
 
-const playerName = askName();
-
-console.log(`Let's play a game, ${playerName}!`);
-
-const numbers = generateNumbers();
-let totalCount = 0;
-
-for (let i = 1; i <= 3; i += 1) {
-  totalCount += question(numbers[i - 1]);
-  if (totalCount !== i) {
-    console.log(`Let's try again, ${playerName}!`);
-    break;
-  }
-}
-
-if (totalCount === 3) {
-  console.log(`Congratulations, ${playerName}!`);
-}
+game(oddOrEven.game, oddOrEven.generate);
