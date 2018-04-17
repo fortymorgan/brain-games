9114e0c4d64d3ff7a490ed1586e77eaca7c19293
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-17 10:57:11 +0300

    Set askName function to return name

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import readlineSync from 'readline-sync';
 const askName = () => {
   const name = readlineSync.question('Tell me your name, please: ');
   console.log(`Hello, ${name}!`);
+
+  return name;
 };
 
 export default askName;
