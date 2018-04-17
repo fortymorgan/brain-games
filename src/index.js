c6d090e544207ddd0d52e401cc5fefb558f5916d
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-17 20:22:05 +0300

    Move general game process from game files to index

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ const askName = () => {
   return name;
 };
 
-const game = (gameProcess, generateNumbers) => {
+const gameProcess = (question, numbers, actions) => {
   const playerName = askName();
   console.log(`Let's play a game, ${playerName}!`);
 
-  gameProcess(generateNumbers(), playerName);
+  let totalCount = 0;
+  for (let i = 1; i <= 3; i += 1) {
+    totalCount += question(numbers[i - 1], actions[i - 1]);
+    if (totalCount !== i) {
+      console.log(`Let's try again, ${playerName}!`);
+      break;
+    }
+  }
+
+  if (totalCount === 3) {
+    console.log(`Congratulations, ${playerName}!`);
+  }
 };
 
-export default game;
+export default gameProcess;
