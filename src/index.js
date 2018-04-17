40c71ce28d48be9d5cb0db9347573d9769d987f2
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-17 14:49:57 +0300

    Remove extra greeting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 
 const askName = () => {
   const name = readlineSync.question('Tell me your name, please: ');
-  console.log(`Hello, ${name}!`);
 
   return name;
 };
