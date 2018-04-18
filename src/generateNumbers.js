6cd14c9089880e3b4dca54b7041823b62b9aab1d
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-18 21:48:32 +0300

    Make generatePair return literal

--- src/generateNumbers.js
+++ src/generateNumbers.js
@@ -1,11 +1,3 @@
 export const generateSimple = rate => Math.round((Math.random() * rate) + 1);
 
-export const generatePair = (rate) => {
-  const array = [];
-  for (let j = 0; j < 2; j += 1) {
-    array.push(generateSimple(rate));
-  }
-
-  return array;
-};
-
+export const generatePair = rate => [generateSimple(rate), generateSimple(rate)];
