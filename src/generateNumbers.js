99c3eff952fbed00807d4bccccb1371acce8f5d7
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-18 21:43:42 +0300

    Remove duplicated code

--- src/generateNumbers.js
+++ src/generateNumbers.js
@@ -3,8 +3,9 @@ export const generateSimple = rate => Math.round((Math.random() * rate) + 1);
 export const generatePair = (rate) => {
   const array = [];
   for (let j = 0; j < 2; j += 1) {
-    array.push(Math.round((Math.random() * rate) + 1));
+    array.push(generateSimple(rate));
   }
 
   return array;
 };
+
