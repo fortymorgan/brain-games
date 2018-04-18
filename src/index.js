dba930c93a11032c3d25ebc9387599f6ae7bd402
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-18 15:17:15 +0300

    Chage game process to FULL interplay

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import readlineSync from 'readline-sync';
 
-const askName = () => {
-  const name = readlineSync.question('Tell me your name, please: ');
+const gameProcess = (gameArray) => {
+  console.log('Welcome to the Brain Games!');
 
-  return name;
-};
+  const playerName = readlineSync.question('Tell me your name, please: ');
 
-const gameProcess = (question, numbers, actions) => {
-  const playerName = askName();
   console.log(`Let's play a game, ${playerName}!`);
 
   let totalCount = 0;
+
+  console.log(gameArray[0]);
+
   for (let i = 1; i <= 3; i += 1) {
-    totalCount += question(numbers[i - 1], actions[i - 1]);
-    if (totalCount !== i) {
-      console.log(`Let's try again, ${playerName}!`);
+    console.log(gameArray[i].question);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === gameArray[i].answer) {
+      console.log('Correct!');
+      totalCount += 1;
+    } else {
+      console.log('Wrong!');
       break;
     }
   }
 
   if (totalCount === 3) {
     console.log(`Congratulations, ${playerName}!`);
+  } else {
+    console.log(`Let's try again, ${playerName}!`);
   }
 };
 
