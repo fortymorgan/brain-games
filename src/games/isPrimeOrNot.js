cce14b5e5063006f11ae25f63b31abda0f8c90d0
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-19 21:35:23 +0300

    Introduce prime number game

---
+++ src/games/isPrimeOrNot.js
@@ -0,0 +1,30 @@
+import runGameProcess from '..';
+import { generateSimple } from '../generateNumbers';
+
+const yesIfPrime = (number) => {
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const generateNumber = () => generateSimple(150);
+
+const generateRoundDescription = () => {
+  const value = generateNumber();
+
+  return {
+    question: `Question: ${value}`,
+    answer: yesIfPrime(value),
+  };
+};
+
+const instruction = 'Answer "yes" if number prime otherwise answer "no".';
+
+const runGame = () => {
+  runGameProcess(instruction, generateRoundDescription);
+};
+
+export default runGame;
