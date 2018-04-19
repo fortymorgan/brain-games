c084f9b4aa594877c0427ab4ea4b35a347e3d30a
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-19 19:57:38 +0300

    Introduce generate progression method

--- src/generateNumbers.js
+++ src/generateNumbers.js
@@ -1,3 +1,14 @@
 export const generateSimple = rate => Math.round((Math.random() * rate) + 1);
 
 export const generatePair = rate => [generateSimple(rate), generateSimple(rate)];
+
+export const generateProgression = () => {
+  const progression = [generateSimple(25)];
+  const progressionStep = generateSimple(10);
+
+  for (let i = 0; i < 9; i += 1) {
+    progression.push(progression[i] + progressionStep);
+  }
+
+  return progression;
+};
