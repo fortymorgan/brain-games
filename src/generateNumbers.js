9739c89d5ef9a3dbd9796392e38c747a7820f5e4
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-20 15:30:15 +0300

    Make generate progression based on length

--- src/generateNumbers.js
+++ src/generateNumbers.js
@@ -1,12 +1,12 @@
-export const generateSimple = rate => Math.round((Math.random() * rate) + 1);
+export const generateSimple = rate => Math.floor((Math.random() * rate) + 1);
 
 export const generatePair = rate => [generateSimple(rate), generateSimple(rate)];
 
-export const generateProgression = () => {
+export const generateProgression = (length) => {
   const progression = [generateSimple(25)];
   const progressionStep = generateSimple(10);
 
-  for (let i = 0; i < 9; i += 1) {
+  for (let i = 0; i < length - 1; i += 1) {
     progression.push(progression[i] + progressionStep);
   }
 
